ae5fa0e8709aac1b3b272da2c65f76cf1aade04f
YassenAlmaqtary <67117546+YassenAlmaqtary@users.noreply.github.com>
2022-09-18 14:11:28 +0300

    init

--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
-
-Route::group(['namespace'=>'App\Http\Controllers\Web','prefix'=>'products'], function () {
-
-    Route::get('/{ctegry_id?}', 'ProductWebController@getProduct')->name('product');
+Route::get('/', function () {
+    return view('welcome');
 });
 
-// Route::get('/', function () {
-//     return view('web.product');
-// });
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth'])->name('dashboard');
-
-// require __DIR__.'/auth.php';
+require __DIR__.'/auth.php';
