215bf3e4fb803f93b2e8325a8f21ff79e8e12188
YassenAlmaqtary <67117546+YassenAlmaqtary@users.noreply.github.com>
2022-10-20 19:27:20 +0300

    Update master.blade.php

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -11,9 +11,9 @@
 <div class="wrapper">
 
   <!-- Preloader -->
-  <div class="preloader flex-column justify-content-center align-items-center">
+  {{-- <div class="preloader flex-column justify-content-center align-items-center">
     <img class="animation__shake" src="{{asset('asset/dashboard/assets/dist/img/AdminLTELogo.png')}}" alt="AdminLTELogo" height="60" width="60">
-  </div>
+  </div> --}}
 
    @include('admin.layouts.main-header')
 
