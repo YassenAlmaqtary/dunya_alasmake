cf4bd4650d09904b2de94a01805cc436a8309ea3
YassenAlmaqtary <67117546+YassenAlmaqtary@users.noreply.github.com>
2022-10-22 17:06:41 +0300

    init

--- resources/views/admin/layouts/footer-script.blade.php
+++ resources/views/admin/layouts/footer-script.blade.php
@@ -4,7 +4,7 @@
 <!-- jQuery UI 1.11.4 -->
 <script src="{{asset('asset/dashboard/assets/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<script>
+  <script> 
   $.widget.bridge('uibutton', $.ui.button)
 </script>
 <!-- Bootstrap 4 -->
@@ -16,7 +16,7 @@
 <!-- JQVMap -->
 <script src="{{asset('asset/dashboard/assets/plugins/jqvmap/jquery.vmap.min.js')}}"></script>
 <script src="{{asset('asset/dashboard/assets/plugins/jqvmap/maps/jquery.vmap.usa.js')}}"></script>
-<!-- jQuery Knob Chart -->
+ <!-- jQuery Knob Chart --> 
 <script src="{{asset('asset/dashboard/assets/plugins/jquery-knob/jquery.knob.min.js')}}"></script>
 <!-- daterangepicker -->
 <script src="{{asset('asset/dashboard/assets/plugins/moment/moment.min.js')}}"></script>
@@ -35,8 +35,8 @@
 <script src="{{asset('asset/dashboard/assets/dist/js/pages/dashboard.js')}}"></script>
 
 <!--laravel echo and  socketio-->
-<script>window.laravel_echo_port='{{env("LARAVEL_ECHO_PORT")}}';</script>
+{{-- <script>window.laravel_echo_port='{{env("LARAVEL_ECHO_PORT")}}';</script>
 <script src="http://{{ Request::getHost()}}:{{env('LARAVEL_ECHO_PORT')}}/socket.io/socket.io.js"></script>
 <script src="{{ url('js/app.js') }}" type="text/javascript"></script>
-<script src="{{asset('asset/dashboard/assets/js/subscript-message.js')}}"></script>
-@yield('scripts')
+<script src="{{asset('asset/dashboard/assets/js/subscript-message.js')}}"></script>--}}
+@yield('scripts') 
