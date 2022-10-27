43aa10fc62ec5654f35f00bd9eb737bc04a3c83c
YassenAlmaqtary <67117546+YassenAlmaqtary@users.noreply.github.com>
2022-10-27 17:26:08 +0300

    Update app.php

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'dunaalasmac'),
+    'name' => env('APP_NAME', 'dunaalasmak'),
 
     /*
     |--------------------------------------------------------------------------
