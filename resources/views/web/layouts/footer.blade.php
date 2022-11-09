608647388a65d69dc2dca5c8421f83e963063abf
YassenAlmaqtary <yassen.770355274@gmail.com>
2022-11-09 14:51:45 +0300

    Update footer.blade.php

--- resources/views/web/layouts/footer.blade.php
+++ resources/views/web/layouts/footer.blade.php
@@ -3,20 +3,20 @@
   <div class="footer">
      <div class="container">
         <div class="row">
-           <div class="col-md-4">
-              <div class="inror_box">
+           <div class="col-sm-4">
+              <div class="">
                  <h3>الاهداف</h3>
                  <p>{{App\Models\About::selection()->first()->objectives_details}}</p>
               </div>
            </div>
-           <div class="col-md-4">
-              <div class="inror_box">
+           <div class="col-sm-4">
+              <div class="">
                  <h3>الرؤية</h3>
                  <p>{{App\Models\About::selection()->first()->vision_details}}</p>
               </div>
            </div>
-           <div class="col-md-4">
-              <div class="inror_box">
+           <div class="col-sm-4">
+              <div class="">
                  <h3>نبذة عنا</h3>
                  <p>{{App\Models\About::selection()->first()->Aboutus_details}}</p>
               </div>
