eb3b38251ca132eca27d2ff9a82b747329e25360
YassenAlmaqtary <yassen.770355274@gmail.com>
2022-11-11 12:02:34 +0300

    Update head.blade.php

--- resources/views/web/layouts/head.blade.php
+++ resources/views/web/layouts/head.blade.php
@@ -1,5 +1,9 @@
 <head>
     <!-- basic -->
+    
+    <meta http-equiv='cache-control' content='no-cache'>
+    <meta http-equiv='expires' content='0'>
+    <meta http-equiv='pragma' content='no-cache'>
     <meta name="viewport" content="width=devics-width,initial-scale=1">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
