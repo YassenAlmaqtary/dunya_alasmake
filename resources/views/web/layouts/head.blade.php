beda4cc46a58c293ce41dc6408329648d36aa4c8
YassenAlmaqtary <yassen.770355274@gmail.com>
2022-11-11 18:16:24 +0300

    Update head.blade.php

--- resources/views/web/layouts/head.blade.php
+++ resources/views/web/layouts/head.blade.php
@@ -22,8 +22,8 @@
     <!-- style css -->
     @if (config('app.env') == 'local')
     <link rel="stylesheet" href="{{asset('asset/web/asset/css/style.css')}}">
-    {{-- @else
-    <link rel="stylesheet" href="{{asset(mix('asset/web/asset/css/style.css'),true)}}"> --}}
+    @else
+    <link rel="stylesheet" href="{{asset(mix('asset/web/asset/css/style.css'),true)}}">
     @endif
     <!-- Responsive-->
     <link rel="stylesheet" href="{{asset('asset/web/asset/css/responsive.css')}}">
